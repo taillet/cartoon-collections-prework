528cc81217e2804451fbc8e37c54e02235f20bee
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:06:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-narray = []
+  narray = []
     array.each do |item|
       cheese_types.each do |cheese|
         if item == cheese
