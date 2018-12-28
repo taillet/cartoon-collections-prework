d400a352720194e5a2a29650763a0d970fdfe49c
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:07:17 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |dwarf, i|
+    puts "#{i+1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  narray = array.collect do |call|
+    call << "!"
+    call.capitalize
+  end
+  return narray
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+array.each do |word|
+  if word.size > 4
+    return true
+  else
+    return false
+  end
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  narray = []
+    array.each do |item|
+      cheese_types.each do |cheese|
+        if item == cheese
+          narray << cheese
+        end
+      end
+    end
+    return narray[0]
 end
