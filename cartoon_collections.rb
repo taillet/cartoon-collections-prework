ab2dca38f165ca96b712768568d24be54a31cd5c
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 17:49:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types) == false
+  if array.include?(cheese_types) != true
     return nil
   elsif array.include(cheese_types) == true
     array.each do |list|
